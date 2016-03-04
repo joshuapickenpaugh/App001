02536528f981c39abc12f83bb3ea5fcdd89c3e1d
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-03-04 15:19:56 -0600

    First Commit

---
+++ App001/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace App001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPush01 = new System.Windows.Forms.Button();
+            this.lblBox01 = new System.Windows.Forms.Label();
+            this.txtBox01 = new System.Windows.Forms.TextBox();
+            this.txtBox02 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPush01
+            // 
+            this.btnPush01.Location = new System.Drawing.Point(29, 231);
+            this.btnPush01.Name = "btnPush01";
+            this.btnPush01.Size = new System.Drawing.Size(100, 20);
+            this.btnPush01.TabIndex = 0;
+            this.btnPush01.Text = "PUSH";
+            this.btnPush01.UseVisualStyleBackColor = true;
+            this.btnPush01.Click += new System.EventHandler(this.btnPush01_Click);
+            // 
+            // lblBox01
+            // 
+            this.lblBox01.AutoSize = true;
+            this.lblBox01.Location = new System.Drawing.Point(26, 168);
+            this.lblBox01.Name = "lblBox01";
+            this.lblBox01.Size = new System.Drawing.Size(35, 13);
+            this.lblBox01.TabIndex = 1;
+            this.lblBox01.Text = "label1";
+            // 
+            // txtBox01
+            // 
+            this.txtBox01.Location = new System.Drawing.Point(29, 47);
+            this.txtBox01.Name = "txtBox01";
+            this.txtBox01.Size = new System.Drawing.Size(100, 20);
+            this.txtBox01.TabIndex = 2;
+            // 
+            // txtBox02
+            // 
+            this.txtBox02.Location = new System.Drawing.Point(29, 103);
+            this.txtBox02.Name = "txtBox02";
+            this.txtBox02.Size = new System.Drawing.Size(100, 20);
+            this.txtBox02.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter your name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Year of Birth:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(163, 287);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox02);
+            this.Controls.Add(this.txtBox01);
+            this.Controls.Add(this.lblBox01);
+            this.Controls.Add(this.btnPush01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPush01;
+        private System.Windows.Forms.Label lblBox01;
+        private System.Windows.Forms.TextBox txtBox01;
+        private System.Windows.Forms.TextBox txtBox02;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ App001/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPush01_Click(object sender, EventArgs e)
+        {
+            int x;
+            int total;
+
+            x = Convert.ToInt32(txtBox02.Text);
+            total = 2016 - x;
+
+            lblBox01.Text = txtBox01.Text + " is " + total + " years old.";
+        }
+    }
+}
